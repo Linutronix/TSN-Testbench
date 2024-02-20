b9d39053149afbdbf5d9284d6542ad8774b72c4d
Kurt Kanzenbach <kurt@linutronix.de>
2024-02-20 14:36:16 +0100

    tools: Add a build script
    Add a build script to compile with all options and different compilers to
    spot warnings and/or build failures early.
    
    Signed-off-by: Kurt Kanzenbach <kurt@linutronix.de>
    Reviewed-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

---
+++ tools/ci/build.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Copyright (C) 2024 Linutronix GmbH
+# Author Kurt Kanzenbach <kurt@linutronix.de>
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Compile with different options and compilers.
+#
+
+set -e
+
+COMPILER="gcc clang"
+OPTIONS="WITH_MQTT"
+
+cd `dirname $0`
+
+pushd ../..
+
+for compiler in $COMPILER; do
+  for option in $OPTIONS; do
+    mkdir -p build
+    pushd build
+    echo "Trying 'CC=$compiler cmake -D$option=OFF' ..."
+    CC=$compiler cmake -D$option=OFF ..
+    make -j`nproc`
+    echo "Trying 'CC=$compiler cmake -D$option=ON' ..."
+    CC=$compiler cmake -D$option=ON ..
+    make -j`nproc`
+    popd
+    rm -rf build
+  done
+done
+
+make -C Documentation html
+
+popd
+
+exit 0
