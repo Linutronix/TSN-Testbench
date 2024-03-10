300f75c4cec573a73f68f3a2ac0ed911623eab96
Kurt Kanzenbach <kurt@linutronix.de>
2024-03-10 15:03:25 +0100

    tools: Add a qa script
    Add a qa script to catch coding style errors.
    
    Signed-off-by: Kurt Kanzenbach <kurt@linutronix.de>
    Reviewed-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

---
+++ tools/ci/qa.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Copyright (C) 2024 Linutronix GmbH
+# Author Kurt Kanzenbach <kurt@linutronix.de>
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+# Check coding style.
+#
+
+set -e
+
+cd `dirname $0`
+
+pushd ../..
+
+echo "Checking coding style with clang-format ..."
+clang-format --Werror --dry-run src/*.c src/*.h
+
+echo "Checking naming style with clang-tidy ..."
+mkdir build
+pushd build
+cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+make -j`nproc`
+run-clang-tidy
+popd
+rm -rf build
+
+popd
+
+exit 0
