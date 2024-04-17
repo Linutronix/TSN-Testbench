6fbb8c1fce6a9be628b22ac86b6315d110187015
Kurt Kanzenbach <kurt@linutronix.de>
2024-04-17 12:52:57 +0200

    tests/busypolling_8ms: Add new busypolling test
    This one uses 8ms cycle time. However, the payload size is 512
    bytes. Apparently there are applications using this frame size.
    
    Signed-off-by: Kurt Kanzenbach <kurt@linutronix.de>
    Reviewed-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

---
+++ tests/busypolling_8ms/ref.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Copyright (C) 2024 Linutronix GmbH
+# Author Kurt Kanzenbach <kurt@linutronix.de>
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+set -e
+
+cd "$(dirname "$0")"
+
+# Start PTP
+../../scripts/ptp.sh enp3s0
+sleep 30
+
+# Configure flow
+./flow_cml.sh enp3s0
+sleep 30
+
+# Start one instance of reference application
+cp ../../build/xdp_kern_*.o .
+../../build/reference -c reference_vid100_cml.yaml > ref1.log &
+
+exit 0
