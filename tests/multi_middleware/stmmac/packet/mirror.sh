7866d481bda78a675d8516b75fb1270b2d3eb94f
Kurt Kanzenbach <kurt@linutronix.de>
2024-08-30 16:23:14 +0200

    tests: Relocate multi middleware test
    Relocate multi middleware test to its own stmmac directory. Prepare that
    for adding other tests for Intel i226.
    
    Signed-off-by: Kurt Kanzenbach <kurt@linutronix.de>
    Reviewed-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- tests/multi_middleware/stmmac/packet/mirror.sh
+++ tests/multi_middleware/stmmac/packet/mirror.sh
@@ -11,7 +11,7 @@ set -e
 cd "$(dirname "$0")"
 
 # Start PTP
-../../../scripts/ptp.sh enp0s29f2
+../../../../scripts/ptp.sh enp0s29f2
 sleep 10
 
 # Configure flow
@@ -19,12 +19,12 @@ sleep 10
 sleep 10
 
 # Start four instances of mirror applications
-../../../build/mirror -c mirror_vid100_ehl.yaml >mirror1.log &
+../../../../build/mirror -c mirror_vid100_ehl.yaml >mirror1.log &
 sleep 1
-../../../build/mirror -c mirror_opcua_vid200_ehl.yaml >mirror2.log &
+../../../../build/mirror -c mirror_opcua_vid200_ehl.yaml >mirror2.log &
 sleep 1
-../../../build/mirror -c mirror_opcua_vid300_ehl.yaml >mirror3.log &
+../../../../build/mirror -c mirror_opcua_vid300_ehl.yaml >mirror3.log &
 sleep 1
-../../../build/mirror -c mirror_avtp_vid400_ehl.yaml >mirror4.log &
+../../../../build/mirror -c mirror_avtp_vid400_ehl.yaml >mirror4.log &
 
 exit 0
