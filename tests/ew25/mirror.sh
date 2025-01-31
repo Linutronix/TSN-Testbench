9dd0602d9f02f9f2821b77335bd63066c50180d5
Kurt Kanzenbach <kurt@linutronix.de>
2025-01-31 14:05:38 +0100

    tests: Add config for Embedded World 2025 TSN class
    Add config for Embedded World 2025 TSN class.
    
    Signed-off-by: Kurt Kanzenbach <kurt@linutronix.de>
    Reviewed-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

---
+++ tests/ew25/mirror.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Copyright (C) 2023-2025 Linutronix GmbH
+# Author Kurt Kanzenbach <kurt@linutronix.de>
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+set -e
+
+cd "$(dirname "$0")"
+
+# Start PTP
+../../scripts/ptp.sh enp88s0
+sleep 30
+
+# Configure flow
+./flow.sh enp88s0
+sleep 30
+
+# Start one instance of mirror application
+cp ../../build/xdp_kern_*.o .
+../../build/mirror -c mirror_vid100.yaml
+
+exit 0
